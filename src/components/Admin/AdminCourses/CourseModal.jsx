15573a1cbdc42ae642256c11d19ea43fc7eac510
KoushikDuttaHih7 <koushik.dutta@hih7.in>
2023-04-28 15:25:47 +0530

    #18 ADMIN COURSES PAGE

---
+++ src/components/Admin/AdminCourses/CourseModal.jsx
@@ -0,0 +1,90 @@
+import {
+  Box,
+  Button,
+  Grid,
+  Heading,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+  Stack,
+  Text,
+} from '@chakra-ui/react';
+import React from 'react';
+import { RiDeleteBin7Fill } from 'react-icons/ri';
+
+const CourseModal = ({
+  isOpen,
+  onClose,
+  id,
+  deleteButtonHandler,
+  addLectureHandler,
+  courseTitle,
+  lectures = [],
+}) => {
+  //   const courseTitle = 'React Course';
+  return (
+    <Modal isOpen={isOpen} size="full">
+      <ModalOverlay />
+
+      <ModalContent>
+        <ModalHeader>{courseTitle}</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody p="16">
+          <Grid templateColumns={['1fr', '3fr 1fr']}>
+            <Box px={['0', '16']}>
+              <Box my="5">
+                <Heading children={courseTitle} />
+                <Heading children={`#${id}`} size="sm" opacity={0.4} />
+              </Box>
+              <Heading children={'Lectures'} size="lg" />
+              <VideoCard
+                title="React Intro"
+                description="asrlfgnldj lsrknjglsrgk"
+                num={1}
+                lectureId="457"
+                courseId={id}
+                deleteButtonHandler={deleteButtonHandler}
+              />
+            </Box>
+          </Grid>
+        </ModalBody>
+      </ModalContent>
+    </Modal>
+  );
+};
+
+export default CourseModal;
+
+function VideoCard({
+  title,
+  description,
+  num,
+  lectureId,
+  courseId,
+  deleteButtonHandler,
+}) {
+  return (
+    <Stack
+      direction={['column', 'row']}
+      my="8"
+      borderRadius={'lg'}
+      boxShadow={'0 0 10px rgba(107,70,193,0.5)'}
+      justifyContent={['flex-start', 'space-between']}
+      p={['4', '8']}
+    >
+      <Box>
+        <Heading size={'sm'} children={`#${num} ${title}`} />
+        <Text children={description} />
+      </Box>
+      <Button
+        color={'purple.600'}
+        onClick={() => deleteButtonHandler(courseId, lectureId)}
+      >
+        <RiDeleteBin7Fill />
+      </Button>
+    </Stack>
+  );
+}
