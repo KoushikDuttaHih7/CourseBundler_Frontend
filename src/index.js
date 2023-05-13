8839a9ccb9e345e7956ce8f9fc998d93d9e4a70c
KoushikDuttaHih7 <koushik.dutta@hih7.in>
2023-05-13 12:39:15 +0530

    Delete My Profile

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { ColorModeScript } from '@chakra-ui/react';
-import React, { StrictMode } from 'react';
+import React from 'react';
+import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import App from './App';
 import { ChakraProvider, theme } from '@chakra-ui/react';
